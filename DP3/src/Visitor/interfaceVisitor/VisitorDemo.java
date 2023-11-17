fe0b251fb8b8602161aae3c10e723bdea9cdb4b3
YonghanKwon <kt3155@naver.com>
2023-11-17 15:57:21 +0900

    practice 1

--- DP3/src/Visitor/interfaceVisitor/VisitorDemo.java
+++ DP3/src/Visitor/interfaceVisitor/VisitorDemo.java
@@ -5,10 +5,13 @@ interface ICarElementVisitor {
     void visit(Engine engine);
     void visit(Body body);
     void visit(Car car); 
+    void visit(ICarElement ICarElement);
 }
 
-interface ICarElement {
-    abstract public void accept(ICarElementVisitor visitor);   
+abstract class ICarElement {
+    public void accept(ICarElementVisitor visitor) {
+    	visitor.visit(this);
+    }
 }
 
 public class VisitorDemo {
@@ -19,26 +22,20 @@ public class VisitorDemo {
     }
 }
 
-class Wheel implements ICarElement {
+class Wheel extends ICarElement {
     private String name;
     public Wheel(String name) { this.name = name; } 
     public String getName() { return this.name; }
-    public void accept(ICarElementVisitor visitor) {
-        visitor.visit(this);     
-    }
+
 }
-class Engine implements ICarElement {
-    public void accept(ICarElementVisitor visitor) {
-        visitor.visit(this);     
-    }
+class Engine extends ICarElement {
+	
 }
-class Body implements ICarElement {
-    public void accept(ICarElementVisitor visitor) {
-        visitor.visit(this);     
-    }
+class Body extends ICarElement {
+
 }
 
-class Car implements ICarElement {
+class Car extends ICarElement {
     ICarElement[] elements;
     public Car() {
         this.elements = new ICarElement[] { new Wheel("front left"), new Wheel("front right"), new Wheel("back left") , new Wheel("back right"), new Body(), new Engine() }; 
@@ -63,6 +60,10 @@ class CarElementPrintVisitor implements ICarElementVisitor {
     public void visit(Car car) {      
         System.out.println("Visiting car");     
     }
+
+    public void visit(ICarElement ICarElement) {
+    	System.out.println("Visiting ICarElement");
+    }
 }
 
 class CarElementDoVisitor implements ICarElementVisitor {	
@@ -77,5 +78,11 @@ class CarElementDoVisitor implements ICarElementVisitor {
     public void visit(Car car) {
         System.out.println("Starting my car");
     }
+    public void visit(ICarElement ICarElement) {
+    	System.out.println("Starting ICarElement");
+    }
 }
-
+/*
+ * 상속을 이용하면 모든 타입이 ICarElement형으로만 가능, car에 대한 코드만 의도한 기능으로 동작. 
+ * overriding(dynamic binding) vs overloading(static binding)
+ * */
